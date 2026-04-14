b1c5aa0d48d11c3256a4e8e38d11ab39cba1bb20
Sntos26 <danielbatis1998santos@gmail.com>
2026-04-14 00:00:56 -0300

    Commit inicial: Projeto Iron Gym Laravel com recursos migrados do PHP

---
+++ resources/views/aulas/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Aulas - Iron Gym</title>
+    <link rel="stylesheet" href="{{ asset('css/aula.css') }}">
+</head>
+<body>
+     <section>
+    <h2>Aulas Semanais</h2>
+    <p>Confira nossa programação de aulas e venha participar!</p>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Horário</th>
+          <th>Segunda</th>
+          <th>Terça</th>
+          <th>Quarta</th>
+          <th>Quinta</th>
+          <th>Sexta</th>
+          <th>Sábado</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>06:00 - 07:00</td>
+          <td class="aula">Musculação</td>
+          <td class="aula">Funcional</td>
+          <td class="aula">Musculação</td>
+          <td class="aula">Funcional</td>
+          <td class="aula">Musculação</td>
+          <td class="aula">Crossfit</td>
+        </tr>
+        <tr>
+          <td>08:00 - 09:00</td>
+          <td class="aula">Pilates</td>
+          <td class="aula">Yoga</td>
+          <td class="aula">Pilates</td>
+          <td class="aula">Yoga</td>
+          <td class="aula">Alongamento</td>
+          <td class="aula">Zumba</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AulaController;
+use App\Http\Controllers\NoticiaController;
+use App\Http\Controllers\PagamentoController;
+use App\Http\Controllers\ProdutoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('aulas', AulaController::class);
+Route::resource('noticias', NoticiaController::class);
+Route::resource('pagamentos', PagamentoController::class);
+Route::resource('produtos', ProdutoController::class);
